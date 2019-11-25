39632ac878b54878eded630272e40fe2fa9c9476
ianbag <ian.bagliotti@gmail.com>
2019-11-24 22:06:26 -0300

    create model: quem somos schema

---
+++ src/app/models/quem_somos.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose")
+
+const QuemSomosSchema = new Schema({
+    titulo: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 100
+    },
+    descricao: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        trim: true,
+        maxlength: 1,
+        default: 1
+    }
+},
+    {
+        versionKey: false,
+        timestamps: false
+    }
+)
+module.exports = model('quemSomosSchema', QuemSomosSchema)
