95c9439ff4fb83f650a5e73de89634ad60238a66
hppod <hppodgornik@gmail.com>
2019-11-30 19:50:24 -0200

    Implement routes for faq

---
+++ src/app/routes/faq.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const route = express.Router()
+const FaqController = require('./../controllers/faq')
+
+route.get('/faq', FaqController.get)
+route.post('/faq', FaqController.create)
+route.put('/faq/:id', FaqController.update)
+route.delete('/faq/:id', FaqController.delete)
+
+module.exports = route
