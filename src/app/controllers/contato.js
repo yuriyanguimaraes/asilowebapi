d75c87e89d1fb06bd2d570571baa22ac35c0b771
hppod <hppodgornik@gmail.com>
2019-11-30 18:58:00 -0200

    Implement get method on contato

--- src/app/controllers/contato.js
+++ src/app/controllers/contato.js
@@ -3,6 +3,16 @@ const ContatoModel = require('./../models/contato')
 
 class Contato {
 
+    get(req, res) {
+        ContatoModel.find({}, (contato, err) => {
+            if (err) {
+                res.status(500).send(err)
+            } else {
+                res.status(200).send(contato)
+            }
+        })
+    }
+    
 }
 
 module.exports = new Contato()
