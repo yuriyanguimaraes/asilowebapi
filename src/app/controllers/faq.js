9826f1967e690ed4f4f394fe319b0bda1baf213c
hppod <hppodgornik@gmail.com>
2019-11-30 19:43:45 -0200

    Implement get method for faq and other methods

--- src/app/controllers/faq.js
+++ src/app/controllers/faq.js
@@ -3,6 +3,27 @@ const FaqModel = require('./../models/faq')
 
 class Faq {
 
+    get(req, res) {
+        FaqModel.find({}, (err, faq) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Dados recuperados com sucesso', data: faq })
+            }
+        })
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
 }
 
 module.exports = new Faq()
