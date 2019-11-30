e4635ef9d583734d87dd2ad4a8237056afe101cf
hppod <hppodgornik@gmail.com>
2019-11-30 20:22:00 -0200

    Implement structure quemsomos controller

---
+++ src/app/controllers/quem_somos.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const QuemSomosModel = require('./../models/quem_somos')
+
+class QuemSomos {
+
+    get(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
+}
+
+module.exports = new QuemSomos()
