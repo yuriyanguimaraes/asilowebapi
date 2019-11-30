062048cc1c9fbbf8fc1bf37aa34f649fd4c03ef3
hppod <hppodgornik@gmail.com>
2019-11-30 19:48:22 -0200

    Implement other methods for faq controller

--- src/app/controllers/faq.js
+++ src/app/controllers/faq.js
@@ -14,15 +14,33 @@ class Faq {
     }
 
     create(req, res) {
-
+        FaqModel.create(req.body, (err, faq) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(201).json({ message: 'FAQ inserido com sucesso', data: faq })
+            }
+        })
     }
 
     update(req, res) {
-
+        FaqModel.updateOne({ _id: req.params.id }, { $set: req.body }, (err, faq) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'FAQ atualizado com sucesso', data: faq })
+            }
+        })
     }
 
     delete(req, res) {
-
+        FaqModel.deleteOne({ _id: req.params.id }, (err, faq) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'FAQ apagado com sucesso', data: faq })
+            }
+        })
     }
 }
 
