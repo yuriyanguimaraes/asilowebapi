6a84d1a24de5e37dd2565b1a32053520f95ad94e
hppod <hppodgornik@gmail.com>
2019-11-30 19:53:51 -0200

    Implement structure for historico controller

---
+++ src/app/controllers/historico.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const HistoricoModel = require('./../models/historico')
+
+class Historico {
+
+    get(req, res) {
+
+    }
+
+    create(req, res) {
+
+    }
+
+    update(req, res) {
+
+    }
+
+    delete(req, res) {
+
+    }
+}
+
+module.exports = new Historico()
