8d30b45ac75647917c6b1b6a0f8a2f1977a9fe9e
hppod <hppodgornik@gmail.com>
2019-11-30 19:13:45 -0200

    Config express server to use routes for contato

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
+const ContatoRoutes = require('./src/app/routes/contato')
 
-app.get('/', (req, res) =>{
-    res.send({message: 'API backend node.js para o projeto do site institucional do asilo de ibitinga'})
+app.get('/', (req, res) => {
+    res.send({ message: 'API backend node.js para o projeto do site institucional do asilo de ibitinga' })
 })
 
-app.use('*', (req, res) => res.send({message: 'API não encontrada'}))
+app.use('/', ContatoRoutes)
+
+app.use('*', (req, res) => res.send({ message: 'API não encontrada' }))
 
 app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}!`))
 
