d3b38a1a4870fefe3e3aa7f73d997af1edbe7e0f
hppod <hppodgornik@gmail.com>
2019-11-30 20:28:16 -0200

    Implement quem somos controller

--- src/app/controllers/quem_somos.js
+++ src/app/controllers/quem_somos.js
@@ -4,19 +4,43 @@ const QuemSomosModel = require('./../models/quem_somos')
 class QuemSomos {
 
     get(req, res) {
-
+        QuemSomosModel.find({}, (err, quemSomos) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Dados recuperados com sucesso', data: quemSomos })
+            }
+        })
     }
 
     create(req, res) {
-
+        QuemSomosModel.create(req.body, (err, quemSomos) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(201).json({ message: 'Quem Somos inserido com sucesso', data: quemSomos })
+            }
+        })
     }
 
     update(req, res) {
-
+        QuemSomosModel.updateOne({ _id: req.params.id }, { $set: req.body }, (err, quemSomos) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Quem Somos atualizado com sucesso', data: quemSomos })
+            }
+        })
     }
 
     delete(req, res) {
-
+        QuemSomosModel.deleteOne({ _id: req.params.id }, (err, quemSomos) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Quem Somos apagado com sucesso', data: quemSomos })
+            }
+        })
     }
 }
 
