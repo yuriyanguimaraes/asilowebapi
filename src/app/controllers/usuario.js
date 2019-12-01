f6a90855a26b362337eaaf338214cedb716c12ed
hppod <hppodgornik@gmail.com>
2019-12-01 08:33:08 -0200

    Basic structure for usuario controller

---
+++ src/app/controllers/usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const UsuarioModel = require('./../models/usuario')
+
+class Usuario {
+
+    get(req, res){
+
+    }
+
+    getById(req, res){
+
+    }
+
+    create(req, res){
+
+    }
+
+    update(req, res){
+
+    }
+
+    delete(req, res){
+        
+    }
+}
+
+module.exports = new Usuario()
