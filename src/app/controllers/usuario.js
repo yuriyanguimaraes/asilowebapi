9c7a7a32ad8469baf24f99b3e49e0375c406273b
hppod <hppodgornik@gmail.com>
2019-12-01 08:40:50 -0200

    Implement methods for usuario controller

--- src/app/controllers/usuario.js
+++ src/app/controllers/usuario.js
@@ -3,24 +3,54 @@ const UsuarioModel = require('./../models/usuario')
 
 class Usuario {
 
-    get(req, res){
-
+    get(req, res) {
+        UsuarioModel.find({}, (err, usuario) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Dados recuperados com sucesso', data: usuario })
+            }
+        })
     }
 
-    getById(req, res){
-
+    getById(req, res) {
+        UsuarioModel.findById(req.params.id, (err, usuario) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Usuário recuperado com sucesso', data: usuario })
+            }
+        })
     }
 
-    create(req, res){
-
+    create(req, res) {
+        UsuarioModel.create(req.body, (err, usuario) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(201).json({ message: 'Usuário inserido com sucesso', data: usuario })
+            }
+        })
     }
 
-    update(req, res){
-
+    update(req, res) {
+        UsuarioModel.updateOne({ _id: req.params.id }, { $set: req.body }, (err, usuario) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Usuário atualizado com sucesso', data: usuario })
+            }
+        })
     }
 
-    delete(req, res){
-        
+    delete(req, res) {
+        UsuarioModel.deleteOne({ _id: req.params.id }, (err, usuario) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Usuário apagado com sucesso', data: usuario })
+            }
+        })
     }
 }
 
