d4563af8ef2f0c65a326b4ce471e1d9087891178
yuriyanguimaraes <yuriyanguimaraes@gmail.com>
2020-03-09 19:21:17 -0300

    Faq-paginação-backand

--- src/app/controllers/faq.js
+++ src/app/controllers/faq.js
@@ -3,6 +3,51 @@ const FaqSchema = require('./../models/faq')
 
 class Faq {
 
+    getFaqWithParams(req, res) {
+
+        const limit = 10
+
+        let query = {}
+        let page = req.query.page
+        let skip = limit * (page - 1)
+        let { keyword, order } = req.query
+
+        if (keyword) {
+            query = { $text: { $search: `"\"${keyword}\""` } }
+        }
+
+        FaqSchema
+            .find(query)
+            .sort({ date: order })
+            .skip(skip)
+            .limit(limit)
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).json({ message: 'Houve um erro ao processar sua requisição', err: err })
+                } else if (Array.isArray(data) && data.length == 0) {
+                    res.status(404).json({ message: 'Não foram encontrados dados para os termos da pesquisa! Tente pesquisar novamente' })
+                } else {
+                    FaqSchema
+                        .estimatedDocumentCount()
+                        .find(query)
+                        .exec((err, count) => {
+                            let totalDocuments = count.length
+                            if (err) {
+                                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', err: err })
+                            } else {
+                                res.status(200).json({
+                                    message: 'Dados recuperados com sucesso',
+                                    data: data,
+                                    page: page,
+                                    limit: limit,
+                                    count: totalDocuments
+                                })
+                            }
+                        })
+                }
+            })
+    }
+
     get(req, res) {
         FaqSchema.find({}, (err, faq) => {
             if (err) {
